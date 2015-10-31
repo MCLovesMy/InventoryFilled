f9ba403e1c160567307c6f515693360af3e6d325
MCLovesMy <vandijkendaniel@hotmail.com>
2015-10-31 17:08:56 +0100

    Made a unfinished update for Main.java

--- src/nl/MCLovesMy/Main.java
+++ src/nl/MCLovesMy/Main.java
@@ -1,5 +1,17 @@
 package nl.MCLovesMy;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
 public class Main {
+	//Main Command
+	@Override
+	public boolean oncommand (CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("InventoryFilled")) {
+			
+			return true;
+		}
+		
+	}
 
 }
