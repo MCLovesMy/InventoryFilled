55b3e6ed42f69fe42f6a612c2ceaea7a825773c5
MCLovesMy <vandijkendaniel@hotmail.com>
2015-10-31 19:15:28 +0100

    add enters

---
+++ README.md
@@ -0,0 +1,3 @@
+# InventoryFilled
+A really small Minecraft Spigot Plugin to tell a player his inventory is full.
+
---
+++ src/nl/MCLovesMy/Main.java
@@ -0,0 +1,20 @@
+package nl.MCLovesMy;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements CommandExecutor{
+	//Main Command
+	@Override
+	public boolean onCommand(CommandSender p, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("inventoryfull")) {
+			p.sendMessage(ChatColor.BLUE + "InventoryFull has no commands!");
+		}
+		return false;
+	}
+
+	
+}
