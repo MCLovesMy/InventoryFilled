e6b3e1b2f7379d7cdd04a70cc37d444d1d021cbc
MCLovesMy <vandijkendaniel@hotmail.com>
2015-11-01 13:57:13 +0100

    Version 1.0.0 - Release Ready!
    Set Version to 1.0.0 in plugin.yml.
    Fixed a warning in Main.java.
    All tested! = Ready

--- src/nl/MCLovesMy/Main.java
+++ src/nl/MCLovesMy/Main.java
@@ -2,7 +2,6 @@ package nl.MCLovesMy;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
